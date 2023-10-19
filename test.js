553eed7ce2509a3e193b607887ad1bb24d000b15
suprah925 <141354117+suprah925@users.noreply.github.com>
2023-10-19 14:19:24 -0700

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,14 @@ let answer = string + result'
 console.log("Hello world")
 
 console.log(result)
+
+// Generate a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Check if the number is even or odd
+if (randomNumber % 2 === 0) {
+  console.log(`Random number ${randomNumber} is even.`);
+} else {
+  console.log(`Random number ${randomNumber} is odd.`);
+}
+
