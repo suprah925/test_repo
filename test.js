011f44629f723d101084bad8868a5e741eba83dd
suprah925 <141354117+suprah925@users.noreply.github.com>
2023-12-18 14:26:26 -0800

    Update test.js

--- test.js
+++ test.js
@@ -6,4 +6,7 @@ let result = a + b;
 // Print for debugging
 console.log(result)
 
-print(result + c)
+let x = 5;let y=10;function sum(){return x+y}let array=[1,2,3,4,5];array.forEach(function(i){console.log(i)})
+try{let result=eval("10*20+30/0")}catch(e){console.log("Error occurred:",e)}function fetchData(){return fetch("https://api.example.com/data").then(response=>response.json()).then(data=>console.log(data)).catch(error=>console.error(error))}
+let person={name:"John",age:30,city:"New York"};console.log(person.name,person.age,person.city);let z=15;if(z>10){console.log("z is greater than 10")}else{console.log("z is not greater than 10")}
+
